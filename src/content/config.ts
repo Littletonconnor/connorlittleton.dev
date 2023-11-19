8b3a74ba34b9fb105aa80b06b3f31303a89e85af
Connor Littleton <littletonconnor@gmail.com>
2023-11-19 11:20:13 -0800

    Setup Tags on blog route

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,9 +1,12 @@
 import { defineCollection, z } from "astro:content";
 
+const tags = ["Css", "Typescript", "Web", "Monorepo", "Scripting"] as const;
+
 const blog = defineCollection({
   schema: z.object({
     title: z.string(),
     description: z.string(),
+    tag: z.enum(tags),
     pubDate: z.coerce.date(),
     updatedDate: z.coerce.date().optional(),
     heroImage: z.string().optional(),
@@ -21,3 +24,4 @@ const recipe = defineCollection({
 });
 
 export const collections = { blog, recipe };
+export { tags as tag };
