2fb86d525979a1859f646c126c9021aaf050d1f3
Connor Littleton <littletonconnor@gmail.com>
2023-11-23 21:06:41 -0800

    Fix build errors

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
+import vercel from "@astrojs/vercel/serverless";
 import remarkToc from "remark-toc";
 import { rehypeAccessibleEmojis } from "rehype-accessible-emojis";
 
