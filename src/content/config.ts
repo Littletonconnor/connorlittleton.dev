48b12160530bcdc5ff7e737ad9f0cd5c8ee7ad4f
Connor Littleton <littletonconnor@gmail.com>
2024-01-01 17:32:47 -0800

    setup books config

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,6 +1,6 @@
 import { defineCollection, z } from "astro:content";
 
-const tags = [
+const blogTags = [
   "Css",
   "Typescript",
   "Web",
@@ -9,16 +9,37 @@ const tags = [
   "Recipe",
 ] as const;
 
+const bookTags = [
+  "Economics",
+  "Self-help",
+  "Programming",
+  "Technology",
+  "Business",
+  "Investing",
+  "Science",
+] as const;
+
 const blog = defineCollection({
   schema: z.object({
     title: z.string(),
     description: z.string(),
-    tag: z.enum(tags),
+    tag: z.enum(blogTags),
     pubDate: z.coerce.date(),
     updatedDate: z.coerce.date().optional(),
     heroImage: z.string().optional(),
   }),
 });
 
-export const collections = { blog };
-export { tags as tag };
+const books = defineCollection({
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    tag: z.enum(bookTags),
+    pubDate: z.coerce.date(),
+    updatedDate: z.coerce.date().optional(),
+    heroImage: z.string().optional(),
+  }),
+});
+
+export const collections = { blog, books };
+export { blogTags, bookTags };
