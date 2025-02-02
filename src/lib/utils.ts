405622ece3d76f79be72476bd6562b7a123cd6c7
Connor Littleton <connorlittleton@wealthfront.com>
2025-02-01 16:08:26 -0800

    Some misc blog updates

---
+++ src/lib/utils.ts
@@ -0,0 +1,22 @@
+export const projects = [
+  {
+    name: "Curly",
+    description: "A Curl alternative built with node.",
+    href: "https://github.com/Littletonconnor/curly",
+  },
+  {
+    name: "CWL UI",
+    description: "Myy personal design system library.",
+    href: "https://github.com/Littletonconnor/cwl-ui",
+  },
+  {
+    name: "Connorlittleton.api",
+    description: "a custom JSON placeholder like API.",
+    href: "https://github.com/Littletonconnor/connorlittleton.api",
+  },
+  {
+    name: "CWL Monorepo",
+    description: "A practical monorepo for modern development.",
+    href: "https://github.com/Littletonconnor/cwl-monorepo",
+  },
+];
